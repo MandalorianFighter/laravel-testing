0c2bbdbd2fbee248ab9ac25f4fa841cc59b777bf
MandalorianFighter <yanush.polishchuk@gmail.com>
2023-01-15 16:43:49 +0200

    make tests with unit

---
+++ tests/Unit/CartTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Cart;
+use App\Convert;
+use PHPUnit\Framework\TestCase;
+
+class CartTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_cart_contents()
+    {
+        $cart = new Cart(['Apple']);
+        $this->assertTrue($cart->has('Apple'));
+        $this->assertFalse($cart->has('Dell'));
+    }
+
+    public function test_take_one_from_cart()
+    {
+        $cart = new Cart(['Apple']);
+        $this->assertEquals('Apple', $cart->takeOne());
+        $this->assertNull($cart->takeOne());
+    }
+
+    public function test_price_in_usd()
+    {
+        $price = new Convert(100);
+        $this->assertEquals(113, $price->priceInUsd());
+        $this->assertEquals(83, $price->priceInGbp());
+        $this->assertEquals(144, $price->priceInCad());
+    }
+}
